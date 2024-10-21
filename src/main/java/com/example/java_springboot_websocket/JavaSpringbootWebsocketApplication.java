a07fc4b025aa4840bc6f1dd7af3ed8ec2275b23e
saputhebeast <sapumalwijekoon4@gmail.com>
2024-10-21 22:11:47 +0530

    Feat: initial commit

---
+++ src/main/java/com/example/java_springboot_websocket/JavaSpringbootWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_springboot_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_springboot_websocket/JavaSpringbootWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_springboot_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
